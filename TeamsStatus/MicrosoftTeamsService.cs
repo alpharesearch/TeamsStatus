63624775b17aa972026ec1265282b73504f1781e
Markus Schulz <schulz@alpharesearch.de>
2024-10-24 09:26:22 -0400

    Update MicrosoftTeamsService.cs
    using regex

--- TeamsStatus/MicrosoftTeamsService.cs
+++ TeamsStatus/MicrosoftTeamsService.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging;
 using System.Text;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -31,18 +31,18 @@ namespace TeamsStatus
         //private readonly ILogger<MicrosoftTeamsService> _logger;
         private MicrosoftTeamsStatus _lastStatus = MicrosoftTeamsStatus.Unknown;
         private DateTime _fileLastUpdated = DateTime.MinValue;
-
+        private Regex regex;
         public int PoolingInterval { get; set; }
         public string LogsFilePath { get; set; }
+
 
         public MicrosoftTeamsService() //ILogger<MicrosoftTeamsService> logger
         {
             //_logger = logger;
-
+            this.regex = new Regex("SetBadge Setting badge: NumericBadge\\{\\d\\}, overlay: \\d items, ([A-Za-z]+( [A-Za-z]+)+)", RegexOptions.IgnoreCase);
             this.PoolingInterval = Convert.ToInt32(5);
-            this.LogsFilePath = Environment.ExpandEnvironmentVariables("%localappdata%\\Packages\\MSTeams_8wekyb3d8bbwe\\LocalCache\\Microsoft\\MSTeams\\Logs");
+            this.LogsFilePath = Environment.ExpandEnvironmentVariables("%localappdata%\\Packages\\MSTeams_8wekyb3d8bbwe\\LocalCache\\Microsoft\\MSTeams\\Logs"); //%localappdata%\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\Logs
         }
-
         //public MicrosoftTeamsStatus GetCurrentStatus()
         public String GetCurrentStatus()
         {
@@ -57,72 +57,64 @@ namespace TeamsStatus
                     var lines = ReadLines(fileInfo.FullName);
                     foreach (var line in lines.Reverse())
                     {
-                        var delFrom = @"GlyphBadge{""";
-                        var delTo = @"""}, overlay";
-
-                        if (line.Contains(delFrom) && line.Contains(delTo))
+                        var status = "";
+                        if (this.regex.IsMatch(line))
                         {
-                            int posFrom = line.IndexOf(delFrom) + delFrom.Length;
-                            var info2 = line.Substring(posFrom);
-                            var status2 = info2.Split(@"""}").First();
-                            int posTo = line.IndexOf(delTo);
+                            Match match = this.regex.Match(line);
+                            status = match.Groups[2].Value.Trim(' ');
 
-                            if (true)
+                            var newStatus = _lastStatus;
+
+                            switch (status.ToLower())
                             {
-                                //var info = line.Substring(posFrom, posTo - posFrom);
-                                var status = status2.Split(" -> ").Last();
-                                var newStatus = _lastStatus;
-
-                                switch (status)
-                                {
-                                    case "available":
-                                        newStatus = MicrosoftTeamsStatus.Available;
-                                        return "Green";
-                                        break;
-                                    case "away":
-                                        newStatus = MicrosoftTeamsStatus.Away;
-                                        return "Yellow";
-                                        break;
-                                    case "busy":
-                                    case "onThePhone":
-                                        newStatus = MicrosoftTeamsStatus.Busy;
-                                        return "Red";
-                                        break;
-                                    case "doNotDisturb":
-                                    case "presenting":
-                                        newStatus = MicrosoftTeamsStatus.DoNotDisturb;
-                                        return "Red";
-                                        break;
-                                    case "beRightBack":
-                                        newStatus = MicrosoftTeamsStatus.Away;
-                                        return "Yellow";
-                                        break;
-                                    case "offline":
-                                        newStatus = MicrosoftTeamsStatus.Offline;
-                                        return "Yellow";
-                                        break;
-                                    case "newActivity":
-                                        // ignore this - happens where there is a new activity: Message, Like/Action, File Upload
-                                        // this is not a real status change, just shows the bell in the icon
-                                        break;
-                                    case "inAMeeting":
-                                        newStatus = MicrosoftTeamsStatus.InAMeeting;
-                                        return "Red";
-                                        break;
-                                    default:
-                                        //_logger.LogWarning($"MS Teams status unknown: {status}");
-                                        newStatus = MicrosoftTeamsStatus.Unknown;
-                                        return "NA";
-                                        break;
-                                }
-
-                                if (newStatus != _lastStatus)
-                                {
-                                    _lastStatus = newStatus;
-                                   // _logger.LogInformation($"MS Teams status set to {_lastStatus}");
-                                }
-                                break;
+                                case "available": //SetBadge Setting badge: NumericBadge{2}, overlay: 2 items, status Available
+                                    newStatus = MicrosoftTeamsStatus.Available; //SetBadge Setting badge: NumericBadge{2}, overlay: 2 items, status Available
+                                    return "Green";
+                                    break;
+                                case "away":
+                                    newStatus = MicrosoftTeamsStatus.Away;
+                                    return "Yellow";
+                                    break;
+                                case "busy": //SetBadge Setting badge: NumericBadge{2}, overlay: 2 items, status Busy
+                                case "onThePhone":
+                                    newStatus = MicrosoftTeamsStatus.Busy;
+                                    return "Red";
+                                    break;
+                                case "doNotDisturb":
+                                case "presenting":
+                                    newStatus = MicrosoftTeamsStatus.DoNotDisturb;
+                                    return "Red";
+                                    break;
+                                case "beRightBack":
+                                    newStatus = MicrosoftTeamsStatus.Away;
+                                    return "Yellow";
+                                    break;
+                                case "offline":
+                                    newStatus = MicrosoftTeamsStatus.Offline;
+                                    return "Yellow";
+                                    break;
+                                case "newActivity":
+                                    // ignore this - happens where there is a new activity: Message, Like/Action, File Upload
+                                    // this is not a real status change, just shows the bell in the icon
+                                    break;
+                                case "inAMeeting":
+                                    newStatus = MicrosoftTeamsStatus.InAMeeting;
+                                    return "Red";
+                                    break;
+                                default:
+                                    //_logger.LogWarning($"MS Teams status unknown: {status}");
+                                    newStatus = MicrosoftTeamsStatus.Unknown;
+                                    return "NA";
+                                    break;
                             }
+
+                            if (newStatus != _lastStatus)
+                            {
+                                _lastStatus = newStatus;
+                                // _logger.LogInformation($"MS Teams status set to {_lastStatus}");
+                            }
+                            break;
+                            //}
                         }
                     }
                     _fileLastUpdated = fileLastUpdated;
